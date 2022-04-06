6253e5437e28a45cd57ee9df8897c1f76a9b050e
physci123 <41348538+physci123@users.noreply.github.com>
2022-04-07 00:17:32 +0100

    First attempt

--- stopmotion.py
+++ stopmotion.py
@@ -1,1 +1,19 @@
-#Hello
+#!/usr/bin/env python
+
+from picamera import PiCamera
+from time import sleep
+from gpiozero import Button
+
+button = Button(17)
+camera = PiCamera()
+
+camera.start_preview()
+frame = 1
+while True:
+    try:
+        button.wait_for_press()
+        camera.capture('./animation/frame{:03d}.jpg'.format(frame))
+        frame += 1
+    except KeyboardInterrupt:
+        camera.stop_preview()
+        break
